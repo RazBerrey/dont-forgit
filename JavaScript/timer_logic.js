7a76429c00c8b774c5f93b08695f4ac898f9d034
Forrest Berrey <forrest.berrey@coupa.com>
2017-10-11 09:12:35 -0700

    refactor of original JS logic

--- JavaScript/timer_logic.js
+++ JavaScript/timer_logic.js
@@ -1,17 +1,26 @@
 window.onload = function() {    
 document.getElementById("start").onclick = function(){
+    function countingDown() {
     var timerTil = new Date().getTime()+1500000;
-
+    
+    
     var intervalSet = setInterval(function (){
-    var rightNow = new Date().getTime(); //on interval of 1 second get new "now" time
-    var distance = timerTil - rightNow; //on interval get new distance
+    
+        var rightNow = new Date().getTime(); //rightNowGet()
+   
+        var distance = timerTil - rightNow; //timerRunning()
 
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)); //on interval get new value for minutes slot
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000); //on interval get new value for seconds slot
-    document.getElementById("timer").innerHTML = minutes + "m " + seconds + "s "; //on each interval, take calc'd values and push to html to display to user
-//function below runs each second to determine if distance is at 0 yet, if it is, display message
-    if (distance < 0) {
+   
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)); //minuteFactory()
+   
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000); //secondFactory()
+   
+        document.getElementById("timer").innerHTML = minutes + "m " + seconds + "s "; //displayCounter(minutes, seconds)    
+
+if (distance < 0) {
         clearInterval(intervalSet);
         document.getElementById("timer").innerHTML = "Time's Up";
     } 
-}, 1000)}};
+}, 1000);}
+ }
+};
---
+++ JavaScript/timer_logic_refactor.js
@@ -0,0 +1,56 @@
+window.onload = function() {
+var distance = 0
+var timerTil = 0
+var rightNow = 0
+var minutes = 0
+var seconds = 0
+
+function minuteFactory(timeToFactorM){
+    return Math.floor((timeToFactorM % (1000*60*60)) / (1000*60));
+}
+
+function secondFactory(timeToFactorS){
+    return Math.floor((timeToFactorS % (1000*60)) / 1000);
+}
+
+function timeTilCounter(){
+    timerTil = new Date().getTime()+1500000;
+    return timerTil;
+}
+
+function rightNowGet(){
+    rightNow = new Date().getTime();
+    return rightNow;
+}
+
+function displayCounter(minutes, seconds){
+    document.getElementById("timer").innerHTML = minutes + "m " + seconds + "s ";
+}
+
+function timerRunning(){
+    distance = timerTil - rightNow;
+    return distance
+}
+
+function timerStop(){
+    clearInterval(intervalSet);
+    document.getElementById("timer").innerHTML = "Time's Up";
+}
+var displayMin = minuteFactory(timerRunning());
+var displaySec = secondFactory(timerRunning())
+
+function countDownRunning(){
+    rightNowGet(); //get right now value
+    timerRunning(); // get right now minus timertil value
+    minuteFactory(timerRunning()); //return minute value of distance
+    secondFactory(timerRunning()); //return second value of distance
+    displayCounter(displayMin, displaySec); //push value to html
+}
+
+document.getElementById("start").onclick = function(){
+timeTilCounter(); //return timertil
+var intervalSet = setInterval(function(){
+    countDownRunning()
+},1000)
+};
+}
