1f5c9ac6d701209d35f53f645c57d2859dc8a7e7
Forrest Berrey <forrest.berrey@coupa.com>
2017-10-11 10:18:08 -0700

    replaced original logic with refactored code

--- JavaScript/timer_logic.js
+++ JavaScript/timer_logic.js
@@ -1,26 +1,53 @@
-window.onload = function() {    
+window.onload = function() {
+var distance = 0
+var timerTil = 0
+var rightNow = 0
+var minutes = 0
+var seconds = 0
+
+function minuteFactory(timeToFactorM){
+    return Math.floor((timeToFactorM % (1000*60*60)) / (1000*60));
+}
+
+function secondFactory(timeToFactorS){
+    return Math.floor((timeToFactorS % (1000*60)) / 1000);
+}
+
+function timeTilCounter(){
+    timerTil = new Date().getTime()+1500000;
+    return timerTil;
+}
+
+function rightNowGet(){
+    rightNow = new Date().getTime();
+    return rightNow;
+}
+
+function displayCounter(minutes, seconds){
+    document.getElementById("timer").innerHTML = minutes + "m " + seconds + "s ";
+}
+
+function timerRunning(){
+    return timerTil - rightNow;
+}
+
+function timerStop(){
+    clearInterval(intervalSet);
+    document.getElementById("timer").innerHTML = "Time's Up";
+}
+function countDownRunning(){
+    debugger;
+    rightNowGet(); 
+    var distance = timerRunning(); 
+    var displayMin = minuteFactory(timerRunning()); 
+    var displaySec = secondFactory(timerRunning()); 
+    displayCounter(displayMin, displaySec); 
+}
+
 document.getElementById("start").onclick = function(){
-    function countingDown() {
-    var timerTil = new Date().getTime()+1500000;
-    
-    
-    var intervalSet = setInterval(function (){
-    
-        var rightNow = new Date().getTime(); //rightNowGet()
-   
-        var distance = timerTil - rightNow; //timerRunning()
-
-   
-        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)); //minuteFactory()
-   
-        var seconds = Math.floor((distance % (1000 * 60)) / 1000); //secondFactory()
-   
-        document.getElementById("timer").innerHTML = minutes + "m " + seconds + "s "; //displayCounter(minutes, seconds)    
-
-if (distance < 0) {
-        clearInterval(intervalSet);
-        document.getElementById("timer").innerHTML = "Time's Up";
-    } 
-}, 1000);}
- }
+timeTilCounter(); 
+var intervalSet = setInterval(function(){
+    countDownRunning()
+},1000)
 };
+}
